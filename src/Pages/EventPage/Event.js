37e138c04f315f19abdfb111f62c6c2d37c61406
nilesh <nileshsrivastava.stjoseph@gmail.com>
2026-02-18 16:02:08 +0530

    event page updated (Nileshstack Branch)

--- src/Pages/EventPage/Event.js
+++ src/Pages/EventPage/Event.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./event.css";
 import DomainCard from "../../Components/Domaincard/Domaincard";
 import { Link } from "react-router-dom";
-import technoverse from "../../Assets/technoverse.jpg";
-import sangam from "../../Assets/sangamposter.jpg"
 import websitelaunch1 from "../../Assets/website-launch.jpg";
 import sangam1 from "../../Assets/sangam1.jpg";
 import techno1 from "../../Assets/techno1.jpg";
 import career1 from "../../Assets/careerguide1.jpg";
 import ing1 from "../../Assets/Inga1.jpg";
 import hackers1 from "../../Assets/hh1.jpg";
-
+import innotech1 from "../../Assets/Innotech1.JPG.jpeg";
+import technoverse1 from "../../Assets/technoverse1.jpeg";
+import foundationday from "../../Assets/foundation1.jpeg"
+import uiux1 from "../../Assets/uiux1.jpeg"
 
 
 
@@ -43,6 +44,22 @@ const data = [
     name: "THE HACKER'S MEETUP",
     addon: "date here ",
     img: hackers1,
+  },
+  {
+    name:"Organised Innotech'25",
+    img:innotech1,
+  },
+  {
+    name:"Technoverse 2.0",
+    img:technoverse1,
+  },
+  {
+    name:"2nd foundation day",
+    img:foundationday,
+  },
+  {
+    name:"UI/UX Bootcamp",
+    img:uiux1,
   },
 ];
 
